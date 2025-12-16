29559de941aadc32f846fa217c904751d580ca7c
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2025-12-16 13:39:59 +0000

    Initialize :octocat:

---
+++ lib/sidekiq/sorbet.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "sorbet/version"
+
+module Sidekiq
+  module Sorbet
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
